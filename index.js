ae2cd1a3f5e0d2f0f458b7754506dd48c233c65d
Adrien <adriendesola@gmail.com>
2017-08-18 17:18:20 +0200

    [promise] allways promise

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function (sails) {
     var exec = require('./lib/milight.exec.js');
     var setup = require('./lib/milight.setup.js');
     var init = require('./lib/milight.init.js');
+    var shared = require('./lib/milight.shared.js');
 
     gladys.on('ready', function(){
         setup();
@@ -11,6 +12,7 @@ module.exports = function (sails) {
     return {
         exec,
         setup,
-        init
+        init,
+        shared
     };
 };
