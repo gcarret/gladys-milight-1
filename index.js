06dde5c0032e655de5da009c60b15ef482ea746f
Adrien <adriendesola@gmail.com>
2017-08-20 14:28:15 +0200

    [milight] all commands

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 module.exports = function (sails) {
 
-    var Milight = require('node-milight-promise');
-    var shared = require('./lib/milight.shared.js');
     var exec = require('./lib/milight.exec.js');
     var setup = require('./lib/milight.setup.js');
     var init = require('./lib/milight.init.js');
 
     gladys.on('ready', function(){
-        setTimeout(init, 10000);
+        init()
     });
 
     return {
         exec,
         setup,
-        init,
-        shared,
-        Milight
+        init
     };
 };
