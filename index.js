f1ec85b4fb2c13cb1bc1ff2132a627823059b0a9
Adrien <adriendesola@gmail.com>
2017-08-20 15:13:27 +0200

    lib

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function (sails) {
     var exec = require('./lib/milight.exec.js');
     var setup = require('./lib/milight.setup.js');
     var init = require('./lib/milight.init.js');
-    var shared = require('./milight.shared.js');
+    var shared = require('./lib/milight.shared.js');
 
     gladys.on('ready', function(){
         init()
